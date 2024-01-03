26412ec42abc0d65c331830993d70a8211de16af
aleksandr-gwork <aleksandr.gworkmail@gmail.com>
2024-01-03 11:17:12 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { src, dest, watch, series } = require("gulp");
+const less = require('gulp-less');
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+const concatCss = require("gulp-concat-css");
+
+exports.less = function () {
+  return src("./src/less/*.less")
+    .pipe(less())
+    .pipe(concatCss("style.css"))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./dist/css"));
+};
+
+exports.watch = function () {
+  watch("./src/less/*.less", series("less"));
+};
